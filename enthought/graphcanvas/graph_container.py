de82eed51e574f259e291d8dd8d0ffd34579b5c6
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-13 21:07:32 +0000

    cleaned up code a bit, added weighted example based on NetworkX weighted example

---
+++ enthought/graphcanvas/graph_container.py
@@ -0,0 +1,75 @@
+import networkx
+import numpy
+
+from enthought.enable.api import Container
+from enthought.kiva import CAP_BUTT
+from enthought.traits.api import Instance, Enum
+
+class GraphContainer(Container):
+    """ Enable Container for Directed Acyclic Graphs
+    """
+    
+    bounds = [350, 350]
+    graph = Instance(networkx.Graph)
+    
+    style = Enum('tree', 'shell', 'spring')
+    
+    def do_layout(self, size=None, force=False):
+        """ Nodes of the graph will be layed out based on the the style
+            attribute
+        """ 
+        
+        if self.style == 'tree':
+            layout = networkx.pygraphviz_layout(self.graph, prog='dot')
+            
+            min_x = min([pos[0] for pos in layout.values()])
+            max_y = max([pos[1] for pos in layout.values()])
+            
+            for component in self.components:
+                component.x = layout[component._key][0] - min_x
+                component.y = self.height - max_y + layout[component._key][1]
+        elif self.style == 'shell':
+            layout = networkx.shell_layout(self.graph)
+            for component in self.components:
+                component.y = self.width * (1 + layout[component._key][0])/2
+                component.x = self.height * (1 + layout[component._key][1])/2
+        else:
+            layout = networkx.spring_layout(self.graph)
+            for component in self.components:
+                component.x = self.width * layout[component._key][0]
+                component.y = self.height * layout[component._key][1]
+            
+            
+
+    def draw(self, gc, view_bounds=None, mode="default"):
+        if self._layout_needed:
+            self.do_layout()
+
+        # draw each component first to ensure their position and size
+        # are more or less finalized
+        component_dict = {}
+        for component in self.components:
+            component.draw(gc, view_bounds, mode)
+            component_dict[component.value] = component
+            
+        # draw the connectors
+        for edge in self.graph.edges():
+            orig = component_dict[edge[0]]
+            dest = component_dict[edge[1]]
+            
+            gc.save_state()
+            gc.set_stroke_color((.5,.5,.5))
+            
+            # TODO: expose weighed parameters
+            if self.graph.weighted:
+                weight = self.graph[edge[0]][edge[1]]
+                if weight < 0.5:
+                    phase = 3 * 2.5;
+                    pattern = 3 * numpy.array((5,5))
+                    gc.set_line_dash(pattern,phase)
+                    gc.set_line_cap(CAP_BUTT)
+            
+            gc.move_to(orig.x + orig.width/2, orig.y)
+            gc.line_to(dest.x + dest.width/2, dest.y + dest.height)
+            gc.draw_path()
+            gc.restore_state()
