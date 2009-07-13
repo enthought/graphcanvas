ab62b13d3d13d8ff725f87105c7325f544bde8a1
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-13 20:07:53 +0000

    added basic shell of the project, no real source exists yet

---
+++ setup.py
@@ -0,0 +1,100 @@
+
+"""
+Interactive Graph (network) Visualization
+
+GraphCanvas is an library for interacting with visualizations of complex 
+graphs. The aim is to allow the developer to declare the graph by the
+simplest means and be able to visualize the graph immediately.
+
+For example::
+
+    from enthought.graphcanvas.api import GraphView, graph_from_dict
+    g = {'a':['b'], 'b':['c', 'd'], 'c':[], 'd':[]}
+    GraphView(graph=graph_from_dict(g)).configure_traits()
+
+
+Prerequisites
+-------------
+* `NetworkX <http:://networkx.lanl.gov>`_
+* `setuptools <http://pypi.python.org/pypi/setuptools/0.6c8>`_
+"""
+
+import os
+import zipfile
+
+from distutils import log
+from distutils.command.build import build as distbuild
+from setuptools import setup, Extension, find_packages
+from setuptools.command.develop import develop
+
+
+# FIXME: This works around a setuptools bug which gets setup_data.py metadata
+# from incorrect packages. Ticket #1592
+#from setup_data import INFO
+setup_data = dict(__name__='', __file__='setup_data.py')
+execfile('setup_data.py', setup_data)
+INFO = setup_data['INFO']
+
+# Pull the description values for the setup keywords from our file docstring.
+DOCLINES = __doc__.split("\n")
+
+
+class MyDevelop(develop):
+    def run(self):
+        develop.run(self)
+        self.run_command('build_docs')
+
+class MyBuild(distbuild):
+    def run(self):
+        distbuild.run(self)
+        self.run_command('build_docs')
+
+
+# The actual setup call.
+setup(
+    author = 'Enthought, Inc',
+    author_email = 'info@enthought.com',
+    classifiers = [c.strip() for c in """\
+        Development Status :: 5 - Production/Stable
+        Intended Audience :: Developers
+        Intended Audience :: Science/Research
+        License :: OSI Approved :: BSD License
+        Operating System :: MacOS
+        Operating System :: Microsoft :: Windows
+        Operating System :: OS Independent
+        Operating System :: POSIX
+        Operating System :: Unix
+        Programming Language :: C
+        Programming Language :: Python
+        Topic :: Scientific/Engineering
+        Topic :: Software Development
+        Topic :: Software Development :: Libraries
+        """.splitlines() if len(c.strip()) > 0],
+    cmdclass = {
+        'develop': MyDevelop,
+        'build': MyBuild
+    },
+    description = DOCLINES[1],
+    extras_require = INFO['extras_require'],
+    ext_modules = [],
+    include_package_data = True,
+    install_requires = INFO['install_requires'],
+    license = 'BSD',
+    long_description = '\n'.join(DOCLINES[3:]),
+    maintainer = 'ETS Developers',
+    maintainer_email = 'enthought-dev@enthought.com',
+    name = INFO['name'],
+    namespace_packages = [
+        "enthought",
+        ],
+    packages = find_packages(),
+    platforms = ["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
+    setup_requires = 'setupdocs>=1.0',
+    tests_require = [
+        'nose >= 0.10.3',
+        ],
+    test_suite = 'nose.collector',
+    url = 'http://code.enthought.com/',
+    version = INFO['version'],
+    zip_safe = False,
+    )
---
+++ setup_data.py
@@ -0,0 +1,47 @@
+
+# Function to convert simple ETS project names and versions to a requirements
+# spec that works for both development builds and stable builds.  Allows
+# a caller to specify a max version, which is intended to work along with
+# Enthought's standard versioning scheme -- see the following write up:
+#    https://svn.enthought.com/enthought/wiki/EnthoughtVersionNumbers
+def etsdep(p, min, max=None, literal=False):
+    require = '%s >=%s.dev' % (p, min)
+    if max is not None:
+        if literal is False:
+            require = '%s, <%s.a' % (require, max)
+        else:
+            require = '%s, <%s' % (require, max)
+    return require
+
+
+# Declare our ETS project dependencies.
+ENABLE_TRAITS = etsdep('Enable[traits]', '3.1.1')
+ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.3')
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.1.1')
+TRAITS_UI = etsdep('Traits[ui]', '3.1.1')
+
+
+INFO = {
+    "extras_require": {
+        'wx': [
+            TRAITSBACKENDWX,
+            ],
+
+        # All non-ets dependencies should be in this extra to ensure users can
+        # decide whether to require them or not.
+        'nonets': [
+            'numpy >= 1.1.0',
+            'networkx',
+            #'PyQt4', -- not really required by everyone.
+            #'wxPython', -- not really required by everyone.
+            ],
+        },
+    "install_requires": [
+        ENABLE_TRAITS,
+        ENTHOUGHTBASE,
+        TRAITS_UI,
+        ],
+    "name": 'GraphCanvas',
+    "version": '0.1.0',
+    }
+
