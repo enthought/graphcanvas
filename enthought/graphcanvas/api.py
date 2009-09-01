8a21b2d0b0b31fceb76f07075567da36d252dccd
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-09-01 16:06:38 +0000

    added some imports

--- enthought/graphcanvas/api.py
+++ enthought/graphcanvas/api.py
@@ -1,2 +1,4 @@
 from graph_view import GraphView, graph_from_dict
 from graph_node_component import GraphNodeComponent
+from graph_container import GraphContainer
+from dag_container import DAGContainer
