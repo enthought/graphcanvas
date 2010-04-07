92b52d7aeafc355f810d4742bcf281f1c3f9d544
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-04-07 17:01:39 +0000

    updated example to check for pygraphviz

--- examples/circular_tree.py
+++ examples/circular_tree.py
@@ -1,5 +1,15 @@
 import networkx
 from enthought.graphcanvas.api import GraphView
 
+try:
+    import pygraphviz
+except ImportError:
+    import sys
+    print ''
+    print 'ERROR: circular layout requires pygraphviz: '\
+          'http://networkx.lanl.gov/pygraphviz'
+    print ''
+    sys.exit(2)
+
 g = networkx.balanced_tree(3,5)
 GraphView(graph=g, layout='circular').configure_traits()
