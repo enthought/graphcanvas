9be536a2802583719945e1e2ecd2bc284e3eef4c
stevenjkern <kern.steven0@gmail.com>
2016-09-13 09:47:46 -0500

    Much simpler implementation of drag tool

--- graphcanvas/graph_node_drag_tool.py
+++ graphcanvas/graph_node_drag_tool.py
@@ -1,58 +1,29 @@
-from enable.api import BaseTool, Pointer
-from traits.api import Either, Float, HasTraits
+from enable.tools.api import ValueDragTool
 
-from graph_node_component import GraphNodeComponent
-
-
-class GraphNodeDragTool(BaseTool):
+class GraphNodeDragTool(ValueDragTool):
     """ Listens for a right click on a graph node and allows the user to
         drag the selected node to a desired location. Edges are redrawn
         live during the node's movement.
     """
 
-    dragging_node = Either(GraphNodeComponent, None)
+    drag_button = 'right'
 
-    normal_pointer = Pointer("arrow")
+    x_name = 'x'
 
-    moving_pointer = Pointer("hand")
+    y_name = 'y'
 
-    offset_x = Float
-
-    offset_y = Float
-
-    def dragging_node_default(self):
-        return None
-
-    def normal_right_down(self, event):
+    def get_value(self):
         for node in self.component.components:
-            if node.is_in(event.x, event.y):
-                self.dragging_node = node
-                self.event_state = "moving"
-                event.window.set_pointer(self.moving_pointer)
-                event.window.set_mouse_owner(self, event.net_transform())
-                self.offset_x = event.x - node.x
-                self.offset_y = event.y - node.y
-                event.handled = True
-        return
+            if node.is_in(*self.original_screen_point):
+                print 'node found', node.label
+                return node
 
-    def moving_mouse_move(self, event):
-        self.dragging_node.position = [
-            event.x - self.offset_x, event.y - self.offset_y
-        ]
-        event.handled = True
-        self.dragging_node.request_redraw()
-        return
+    def set_delta(self, value, delta_x, delta_y):
+        if value is not None:
+            value.x = self.original_screen_point[0] + delta_x
+            value.y = self.original_screen_point[1] + delta_y
+            value.request_redraw()
 
-    def moving_right_up(self, event):
-        self.event_state = "normal"
-        event.window.set_pointer(self.normal_pointer)
-        event.window.set_mouse_owner(None)
-        event.handled = True
-        self.dragging_node.request_redraw()
-        self.dragging_node = None
-        return
-
-    def moving_mouse_leave(self, event):
-        self.moving_right_up(event)
-        event.handled = True
-        return
+    def dragging(self, event):
+        event.window.set_pointer("hand")
+        super(GraphNodeDragTool, self).dragging(event)
