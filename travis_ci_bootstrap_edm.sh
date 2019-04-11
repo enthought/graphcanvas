deb6d4bb59fd772127db0a9ad21bb5017ad4d76a
Steven Kern <skern@enthought.com>
2019-04-11 13:18:50 -0500

    FIX: set --platform option on linux to get rh6 env

--- travis_ci_bootstrap_edm.sh
+++ travis_ci_bootstrap_edm.sh
@@ -11,7 +11,7 @@ then
     sudo installer -pkg edm_${EDM_VER}.0.pkg -target /
 else
     # download and install EDM
-    wget https://package-data.enthought.com/edm/rh6_x86_64/${EDM_VER}/edm_${EDM_VER}.0_linux_x86_64.sh
+    wget https://package-data.enthought.com/edm/rh5_x86_64/${EDM_VER}/edm_${EDM_VER}.0_linux_x86_64.sh
     chmod u+x edm_${EDM_VER}.0_linux_x86_64.sh
     ./edm_${EDM_VER}.0_linux_x86_64.sh -b -p ~
     export PATH="~/bin:${PATH}"
