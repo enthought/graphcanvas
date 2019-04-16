1c4e9cb161f9fdac56914be5093059d368425a8d
Steven Kern <skern@enthought.com>
2019-04-16 09:46:10 -0500

    FIX: add the edm executable to the PATH

--- travis_ci_bootstrap_edm.sh
+++ travis_ci_bootstrap_edm.sh
@@ -14,4 +14,5 @@ else
     wget https://package-data.enthought.com/edm/rh5_x86_64/${EDM_VER}/edm_${EDM_VER}.0_linux_x86_64.sh
     chmod u+x edm_${EDM_VER}.0_linux_x86_64.sh
     ./edm_${EDM_VER}.0_linux_x86_64.sh -b -p "${HOME}/edm"
+    export PATH="${HOME}/edm/bin:${PATH}"
 fi
